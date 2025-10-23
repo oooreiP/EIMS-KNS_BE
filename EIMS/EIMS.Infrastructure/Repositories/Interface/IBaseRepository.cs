a3a41f3620b0d13e1b845f3dacbf768deb38e33d
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-10-23 14:52:15 +0700

    .

---
+++ EIMS/EIMS.Infrastructure/Repositories/Interface/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Infrastructure.Repositories.Interface
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        Task<T> GetByIdAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<int> SaveChangesAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, string includeProperties = "");
+        IQueryable<T> GetAllQueryable(string includeProperties = "");
+    }
+}
