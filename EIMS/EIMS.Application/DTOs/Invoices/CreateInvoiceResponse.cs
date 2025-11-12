a6ec4191608f1fb2237632a1a885da1f1a5f9168
Nguyen Viet Man <vietman151@gmail.com>
2025-11-12 14:58:53 +0700

    fix merge conflict

--- EIMS/EIMS.Application/DTOs/Invoices/CreateInvoiceResponse.cs
+++ EIMS/EIMS.Application/DTOs/Invoices/CreateInvoiceResponse.cs
@@ -12,6 +12,7 @@ namespace EIMS.Application.DTOs.Invoices
         public int CustomerID { get; set; }
         public decimal TotalAmount { get; set; }
         public string TotalAmountInWords { get; set; }
+        public string XmlUrl { get; set; }
         public string Status { get; set; }
     }
 }
