45be1a81a1de7e0be4ed3afc25dda94e063f2e2e
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-12-08 21:04:47 +0700

    Hung làm xong EmailService

---
+++ EIMS/EIMS.Application/Features/Emails/Commands/CreateEmailTemplateCommand.cs
@@ -0,0 +1,28 @@
+﻿using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Application.Features.Emails.Commands
+{
+    public class CreateEmailTemplateCommand : IRequest<Result<int>>
+    {
+        [Required]
+        [StringLength(50)]
+        public string TemplateCode { get; set; } 
+
+        [Required]
+        public string LanguageCode { get; set; } = "vi";
+
+        [Required]
+        public string Subject { get; set; }
+
+        public string BodyContent { get; set; } = ""; 
+        public string? Description { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
