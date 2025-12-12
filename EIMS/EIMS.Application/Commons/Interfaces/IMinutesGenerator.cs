364931c8c4d7ba65a1f6059b7ffe85dece464272
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-12-12 19:32:04 +0700

    .

---
+++ EIMS/EIMS.Application/Commons/Interfaces/IMinutesGenerator.cs
@@ -0,0 +1,16 @@
+﻿using EIMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Application.Commons.Interfaces
+{
+    public interface IMinutesGenerator
+    {
+        Task<byte[]> GenerateReplacementMinutesAsync(Invoice invoice, string reason, string contentBefore, string contentAfter, string adjustmentNumber,  DateTime agreementDate);
+        Task<byte[]> GenerateAdjustmentMinutesAsync(Invoice invoice, string reason, string contentBefore, string contentAfter, string adjustmentNumber, DateTime agreementDate);
+
+    }
+}
---
+++ EIMS/EIMS.Domain/Enums/MinutesType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Domain.Enums
+{
+    public enum MinutesType
+    {
+        Replacement = 1, // Thay thế
+        Adjustment = 2   // Điều chỉnh
+    }
+}
