d8e1f090a391ebcff901ced267e5e1eb77f94e48
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-12-19 22:29:43 +0700

    GetPaymentByInvoice

---
+++ EIMS/EIMS.Application/Features/InvoicePayment/Queries/GetPaymentsByInvoiceIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using EIMS.Application.DTOs.InvoicePayment;
+using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Application.Features.InvoicePayment.Queries
+{
+    public class GetPaymentsByInvoiceIdQuery : IRequest<Result<List<InvoicePaymentDTO>>>
+    {
+        public int InvoiceId { get; set; }
+
+        public GetPaymentsByInvoiceIdQuery(int invoiceId)
+        {
+            InvoiceId = invoiceId;
+        }
+    }
+}
