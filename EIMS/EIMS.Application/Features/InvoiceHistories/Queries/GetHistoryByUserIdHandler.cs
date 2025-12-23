7eb93638a4fb77b8cc4cffdcffe8f134609f0f6a
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-12-23 22:33:03 +0700

    them invoice History

---
+++ EIMS/EIMS.Application/Features/InvoiceHistories/Queries/GetHistoryByUserIdHandler.cs
@@ -0,0 +1,41 @@
+﻿using EIMS.Application.Commons.Interfaces;
+using EIMS.Application.DTOs.InvoiceHistories;
+using FluentResults;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Application.Features.InvoiceHistories.Queries
+{
+    public class GetHistoryByUserIdHandler : IRequestHandler<GetHistoryByUserIdQuery, Result<List<InvoiceHistoryDto>>>
+    {
+        private readonly IUnitOfWork _uow;
+
+        public GetHistoryByUserIdHandler(IUnitOfWork uow) => _uow = uow;
+
+        public async Task<Result<List<InvoiceHistoryDto>>> Handle(GetHistoryByUserIdQuery request, CancellationToken token)
+        {
+            var list = await _uow.InvoiceHistoryRepository.GetAllQueryable()
+                .Where(x => x.PerformedBy == request.UserId)
+                .OrderByDescending(x => x.Date)
+                .Include(x => x.Performer)
+                .Select(x => new InvoiceHistoryDto
+                {
+                    HistoryID = x.HistoryID,
+                    InvoiceID = x.InvoiceID,
+                    ActionType = x.ActionType,
+                    Date = x.Date,
+                    PerformedBy = x.PerformedBy,
+                    PerformerName = x.Performer.FullName,
+                    ReferenceInvoiceID = x.ReferenceInvoiceID
+                })
+                .ToListAsync(token);
+
+            return Result.Ok(list);
+        }
+    }
+}
--- EIMS/EIMS.Application/Features/Invoices/Commands/SignInvoice/SignInvoiceCommandHandler.cs
+++ EIMS/EIMS.Application/Features/Invoices/Commands/SignInvoice/SignInvoiceCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using EIMS.Application.Commons;
 using EIMS.Application.Commons.Interfaces;
+using EIMS.Domain.Constants;
+using EIMS.Domain.Entities;
 using FluentResults;
 using MediatR;
 using System;
@@ -67,6 +69,14 @@ namespace EIMS.Application.Features.Invoices.Commands.SignInvoice
             invoice.DigitalSignature = signedXmlContent.SignatureValue;
             invoice.SignDate = DateTime.UtcNow;
             await _unitOfWork.InvoicesRepository.UpdateAsync(invoice);
+            var history = new InvoiceHistory
+            {
+                InvoiceID = request.InvoiceId,
+                ActionType = InvoiceActionTypes.Signed,
+                PerformedBy = invoice.IssuerID,
+                Date = DateTime.UtcNow
+            };
+            await _unitOfWork.InvoiceHistoryRepository.CreateAsync(history);
             await _unitOfWork.SaveChanges();
 
             return Result.Ok();
