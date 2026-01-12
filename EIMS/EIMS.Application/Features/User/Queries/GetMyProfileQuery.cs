70193dc804c0fe1a53466e3979f76309ed201732
hungmegame123fpt <hungthinh26072004@gmail.com>
2026-01-13 00:56:48 +0700

    .

---
+++ EIMS/EIMS.Application/Features/User/Queries/GetMyProfileQuery.cs
@@ -0,0 +1,15 @@
+﻿using EIMS.Application.DTOs.User;
+using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Application.Features.User.Queries
+{
+    public class GetMyProfileQuery : IRequest<Result<UserResponse>>
+    {
+    }
+}
