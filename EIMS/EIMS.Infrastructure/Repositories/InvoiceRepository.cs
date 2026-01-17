2e9e3c265cb8902f72e22663b4b699756b11ce83
Nguyen Viet Man <vietman151@gmail.com>
2026-01-17 21:23:47 +0700

    fix : outstanding

--- EIMS/EIMS.Infrastructure/Repositories/InvoiceRepository.cs
+++ EIMS/EIMS.Infrastructure/Repositories/InvoiceRepository.cs
@@ -505,8 +505,8 @@ namespace EIMS.Infrastructure.Repositories
             };
 
             if (metrics.NetRevenue > 0)
-                metrics.CollectionRate = Math.Round(((double)metrics.CashCollected / (double)metrics.NetRevenue) * 100, 2);
-            metrics.OutstandingRate = Math.Round(((double)metrics.Outstanding / (double)metrics.NetRevenue) * 100, 2);
+                metrics.CollectionRate = Math.Round((double)metrics.CashCollected / (double)metrics.NetRevenue * 100, 2);
+            metrics.OutstandingRate = Math.Round((double)metrics.Outstanding / (double)metrics.NetRevenue * 100, 2);
 
             // B. CASH FLOW (6 Tháng gần nhất)
             var cashFlowRaw = await _context.Invoices
