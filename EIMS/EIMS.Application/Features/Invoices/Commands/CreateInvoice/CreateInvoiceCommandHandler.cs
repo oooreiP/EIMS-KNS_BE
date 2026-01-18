ebebe46e6ccc7ae5439e57c2d67f43601f34161f
Nguyen Viet Man <vietman151@gmail.com>
2026-01-18 19:34:19 +0700

    fix : add last modify date and change createdby invoice creationn

--- EIMS/EIMS.Application/Features/Invoices/Commands/CreateInvoice/CreateInvoiceCommandHandler.cs
+++ EIMS/EIMS.Application/Features/Invoices/Commands/CreateInvoice/CreateInvoiceCommandHandler.cs
@@ -142,7 +142,7 @@ namespace EIMS.Application.Features.Invoices.Commands.CreateInvoice
                     PaymentStatusID = 1,
                     PaymentDueDate = DateTime.UtcNow.AddDays(30),
                     IssuerID = null,
-                    CreatedBy = request.PerformedBy,
+                    CreatedBy = request.AuthenticatedUserId,
                     MinRows = request.MinRows ?? 5,
                     PaidAmount = 0,
                     RemainingAmount = totalAmount,
