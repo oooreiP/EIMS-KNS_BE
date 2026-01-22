a78bd303bbd6268b1314646e889af634d4adf8f9
Nguyen Viet Man <vietman151@gmail.com>
2026-01-23 01:40:34 +0700

    fix : add link login to register, reset password account

--- EIMS/EIMS.Application/Features/Authentication/Commands/RegisterCommandHandler.cs
+++ EIMS/EIMS.Application/Features/Authentication/Commands/RegisterCommandHandler.cs
@@ -113,7 +113,7 @@ namespace EIMS.Application.Features.Authentication.Commands
             <p><strong>Email:</strong> {request.Email}</p>
             <p><strong>Temporary Password:</strong> {tempPassword}</p>
             <p>Please log in and change your password immediately.</p>
-            <a href='http://your-frontend-url/login'>Click here to Login</a>";
+            <a href='https://www.knsinvoice.id.vn'>Click here to Login</a>";
 
             var mailRequest = new DTOs.Mails.MailRequest
             {
