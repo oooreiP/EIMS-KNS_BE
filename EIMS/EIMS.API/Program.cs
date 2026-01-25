0c85145b0dcf40a55613aebcc72ad83a551d5623
Nguyen Viet Man <vietman151@gmail.com>
2026-01-26 06:04:47 +0700

    fix : change signalR

--- EIMS/EIMS.API/Program.cs
+++ EIMS/EIMS.API/Program.cs
@@ -55,6 +55,22 @@ builder.Services
     .AddJwtBearer(options =>
     {
         options.SaveToken = true;
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+
+                if (!string.IsNullOrEmpty(accessToken)
+                    && path.StartsWithSegments("/hubs/notifications"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
