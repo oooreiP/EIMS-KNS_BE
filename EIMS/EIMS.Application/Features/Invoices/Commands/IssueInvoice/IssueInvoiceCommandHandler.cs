b4507e479c75d855f8aad642ba9b07e856cfd421
Nguyen Viet Man <vietman151@gmail.com>
2026-01-25 20:12:24 +0700

    fix : calculation logic

--- EIMS/EIMS.Application/Features/Invoices/Commands/IssueInvoice/IssueInvoiceCommandHandler.cs
+++ EIMS/EIMS.Application/Features/Invoices/Commands/IssueInvoice/IssueInvoiceCommandHandler.cs
@@ -86,18 +86,27 @@ namespace EIMS.Application.Features.Invoices.Commands.IssueInvoice
                 {
                     initialPaidAmount += invoice.Payments.Sum(x => x.AmountPaid);
                 }
-                invoice.PaidAmount = initialPaidAmount;
-                invoice.RemainingAmount = invoice.TotalAmount - initialPaidAmount;
-                // Cập nhật trạng thái
-                // Nếu Remaining <= 0 nghĩa là đã trả đủ hoặc trả thừa -> Fully Paid (3)
-                if (invoice.RemainingAmount <= 0)
+                if (invoice.TotalAmount < 0)
                 {
+                    invoice.PaidAmount = 0;
+                    invoice.RemainingAmount = 0;
                     invoice.PaymentStatusID = 3;
                 }
                 else
                 {
-                    // Nếu > 0: Đã trả 1 ít -> Partially (2), Chưa trả gì -> Unpaid (1)
-                    invoice.PaymentStatusID = (invoice.PaidAmount > 0) ? 2 : 1;
+                    invoice.PaidAmount = initialPaidAmount;
+                    invoice.RemainingAmount = invoice.TotalAmount - initialPaidAmount;
+                    // Cập nhật trạng thái
+                    // Nếu Remaining <= 0 nghĩa là đã trả đủ hoặc trả thừa -> Fully Paid (3)
+                    if (invoice.RemainingAmount <= 0)
+                    {
+                        invoice.PaymentStatusID = 3;
+                    }
+                    else
+                    {
+                        // Nếu > 0: Đã trả 1 ít -> Partially (2), Chưa trả gì -> Unpaid (1)
+                        invoice.PaymentStatusID = (invoice.PaidAmount > 0) ? 2 : 1;
+                    }
                 }
                 if (invoice.PaymentStatusID == 0) invoice.PaymentStatusID = 1;
                 if (string.IsNullOrEmpty(invoice.LookupCode))
